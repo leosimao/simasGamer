074a3f73286e1b636c9890f9700c92c2ea501e15
Leonardo <leonardosc.me@gmail.com>
2024-04-10 17:15:17 -0300

    Iniciando Projeto SimasGamer Kotlin

---
+++ src/main/kotlin/Jogo.kt
@@ -0,0 +1,6 @@
+import com.google.gson.annotations.SerializedName
+
+class Jogo {
+    @SerializedName("title") val nomeJogo: String? = null
+    @SerializedName("thumb") val imagemJogo: String? = null
+}
